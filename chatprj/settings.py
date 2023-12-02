ccda2719d7e9c263daedfbf0976074b599408061
ryowuandjanet <ryowu0329@gmail.com>
2023-12-02 23:46:33 +0800

    Part 02-Custom Admin

--- chatprj/settings.py
+++ chatprj/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -130,3 +131,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+  "site_title": "Chat App",
+  "site_header": "Chat App",
+  "site_brand": "Chat",
+  "site_logo": "static/image/favicon.png",
+  "copyright": "chatapp.com"
+}
